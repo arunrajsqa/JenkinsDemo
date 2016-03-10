893d6e28e1fbe4335e4879700e775051c06237f3
arunrajsqa <arunrajsqa@gmail.com>
2016-03-10 12:55:54 +0530

    Upload Files

---
+++ src/DemoJenkins/JenkinsDemo.java
@@ -0,0 +1,28 @@
+package DemoJenkins;
+
+import org.testng.annotations.Test;
+
+public class JenkinsDemo {
+	  
+
+		
+	
+	@Test
+    public void SampleTest(){
+             int a = 10;
+             float b = (float) 20.3;
+             double c = 3.14785;
+            System. out.println("The Value of a:" +a );
+            System. out.println("The Value of b:" +b );
+            System. out.println("The Value of c:" +c );
+                          
+         }
+	
+
+
+	
+
+
+
+}
+
